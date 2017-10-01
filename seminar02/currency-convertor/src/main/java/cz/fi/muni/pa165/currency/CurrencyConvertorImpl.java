eeccd3d05250e7c773f1e6cf7048349c3c9625e4
Zbyna <Zbynek.Mlcak@seznam.cz>
2017-10-01 22:07:28 +0200

    seminar02

--- seminar02/currency-convertor/src/main/java/cz/fi/muni/pa165/currency/CurrencyConvertorImpl.java
+++ seminar02/currency-convertor/src/main/java/cz/fi/muni/pa165/currency/CurrencyConvertorImpl.java
@@ -1,6 +1,11 @@
 package cz.fi.muni.pa165.currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Currency;
 
 
@@ -20,7 +25,29 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.trace(String.format("Using CurrencyConvertorImpl.convert(%s, %s, %s)", sourceCurrency, targetCurrency, sourceAmount));
+        if(sourceCurrency == null)
+            throw new IllegalArgumentException("sourceCurrency must not be null.");
+        if(targetCurrency == null)
+            throw new IllegalArgumentException("targetCurrency must not be null.");
+        if(sourceAmount == null)
+            throw new IllegalArgumentException("sourceAmount must not be null.");
+
+        BigDecimal rate;
+        try {
+            rate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+        } catch (ExternalServiceFailureException e) {
+            logger.error(String.format("ExchangeRateTable.getExchangeRate(%s, %s) failed with message: \"%s\"", sourceCurrency, targetCurrency, e.getMessage()));
+            throw new UnknownExchangeRateException(e.getMessage(), e);
+        }
+
+        if(rate == null) {
+            logger.warn(String.format("Exchange rate from %s to %s was not found.", sourceCurrency, targetCurrency));
+            throw new UnknownExchangeRateException("Exchange rate not found.");
+        }
+
+        return rate.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
     }
 
 }
